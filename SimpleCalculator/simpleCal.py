db2cea4e2fd1f8f7445d1c2652a14c8e93202c26
CHAQ-89 <chaq1289@gmail.com>
2021-10-22 20:17:07 +0700

    changes-3

--- SimpleCalculator/simpleCal.py
+++ SimpleCalculator/simpleCal.py
@@ -1,4 +1,5 @@
 from tkinter import *
+import parser
 root = Tk()
 root.title("Simple Calculator")
 e = Entry(root)
@@ -19,7 +20,6 @@ def get_number(number):
 
 
 def equal():
-    pass
     # Lay gia tri tu` entry
     #s = e.get()
     # Tach chuoi~ so' truoc va sau dau '+'
@@ -32,14 +32,21 @@ def equal():
     # e.delete(0, END)
     # # Hien thi ket qua
     # e.insert(0, first_number + int(second_num))
+    entries_string = e.get()
+    comp = parser.expr(entries_string).compile()
+    result = eval(comp)
+    clear()
+    e.insert(0, result)
 
 
-def button_adds():
-    pass
+def get_operation(operation):
     #first_num = e.get()
     # global first_number
     # first_number = int(first_num)
     # e.delete(0, END)
+    global i
+    e.insert(i, operation)
+    i += 1
 
 
 def clear():
@@ -53,7 +60,8 @@ Button(root, text="8", command=lambda: get_number(8)).grid(
     row=2, column=1, sticky=N+S+E+W)
 Button(root, text="7", command=lambda: get_number(7)).grid(
     row=2, column=0, sticky=N+S+E+W)
-Button(root, text="+").grid(row=2, column=3, sticky=N+S+E+W)
+Button(root, text="+", command=lambda: get_operation("+")
+       ).grid(row=2, column=3, sticky=N+S+E+W)
 
 Button(root, text="6", command=lambda: get_number(6)).grid(
     row=3, column=2, sticky=N+S+E+W)
@@ -61,7 +69,8 @@ Button(root, text="5", command=lambda: get_number(5)).grid(
     row=3, column=1, sticky=N+S+E+W)
 Button(root, text="4", command=lambda: get_number(4)).grid(
     row=3, column=0, sticky=N+S+E+W)
-Button(root, text="-").grid(row=3, column=3, sticky=N+S+E+W)
+Button(root, text="-", command=lambda: get_operation("-")
+       ).grid(row=3, column=3, sticky=N+S+E+W)
 
 Button(root, text="3", command=lambda: get_number(3)).grid(
     row=4, column=2, sticky=N+S+E+W)
@@ -69,12 +78,15 @@ Button(root, text="2", command=lambda: get_number(2)).grid(
     row=4, column=1, sticky=N+S+E+W)
 Button(root, text="1", command=lambda: get_number(1)).grid(
     row=4, column=0, sticky=N+S+E+W)
-Button(root, text="*").grid(row=4, column=3, sticky=N+S+E+W)
+Button(root, text="*", command=lambda: get_operation("*")
+       ).grid(row=4, column=3, sticky=N+S+E+W)
 
 Button(root, text="C", command=clear).grid(row=5, column=0, sticky=N+S+E+W)
 Button(root, text="0", command=lambda: get_number(0)).grid(
     row=5, column=1, sticky=N+S+E+W)
-Button(root, text="=").grid(row=5, column=2, sticky=N+S+E+W)
-Button(root, text="/").grid(row=5, column=3, sticky=N+S+E+W)
+Button(root, text="=", command=equal).grid(
+    row=5, column=2, sticky=N+S+E+W)
+Button(root, text="/", command=lambda: get_operation("/")
+       ).grid(row=5, column=3, sticky=N+S+E+W)
 
 root.mainloop()
